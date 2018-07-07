050d297f90b308c74fe2d696e8a8f2422c2e5879
wangqiao <2337523303@qq.com>
2018-07-08 00:07:39 +0800

    添加hero功能完成

--- src/views/heroes/add.vue
+++ src/views/heroes/add.vue
@@ -1,24 +1,47 @@
 <template>
-     <div>
-        <h2 class="sub-header">添加 Hero</h2>
+    <div>
+        <h2 class="sub-header">Add Hero</h2>
         <form>
             <div class="form-group">
                 <label for="name">英雄名称</label>
-                <input  type="text" class="form-control" id="name" placeholder="Name">
+                <input v-model="formdata.name " type="text" class="form-control" id="name" placeholder="Name">
             </div>
             <div class="form-group">
                 <label for="sex">英雄性别</label>
-                <input type="text" class="form-control" id="sex" placeholder="Sex">
+                <input v-model="formdata.gender " type="text" class="form-control" id="sex" placeholder="Sex">
             </div>
             <!--  点击提交按钮时，发送请求  -->
-            <button type="submit" class="btn btn-success" @click="handleAdd">Submit</button>
+            <button type="submit" class="btn btn-success" @click.prevent="handleAdd">Submit</button>
         </form>
     </div>
 </template>
 
 <script>
+// 引入axios
+import axios from 'axios'
 export default {
-
+    data() {
+        return {
+            // 双向绑定表单数据，一个对象的格式，方便post传递数据
+            formdata: {
+                name : '',
+                gender : ''
+            }
+        }
+    },
+    methods: {
+        // 点击提交按钮的处理函数
+        handleAdd() {
+            axios
+                .post('http://localhost:3000/heroes',this.formdata)
+                .then((res) => {
+                    if(res.status == 201) {
+                        // 添加成功后跳转到列表页面
+                        this.$router.push({ name: 'heroes' });
+                    }
+                })
+        }
+    }
 }
 </script>
 
